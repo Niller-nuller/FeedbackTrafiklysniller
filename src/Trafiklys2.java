a85541eb354b1e459f88aa35402b3466ccd72871
niels <u1537530303@gmail.com>
2025-09-08 14:21:49 +0200

    Updated

--- src/Trafiklys2.java
+++ src/Trafiklys2.java
@@ -2,8 +2,41 @@ import java.util.Scanner;
 
 public class Trafiklys2 {
     public static void main(String[] args) {
+        int nTime;
+        int nTraf;
+        int nArea;
+        boolean nEmc;
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Indskriv tiden på dage 0-24");
+
+        System.out.print("Indksriv: 1Dag, 2aften eller nat");
+        int uTime = input.nextInt();
+
+//        System.out.println("Indskriv trafik mængde lav eller hæj");
+//        String uTraf = input.nextLine();
+//
+//        System.out.println("Indskriv område. Bolig, Erhver eller Blandet");
+//        String uArea = input.nextLine();
+//
+//        System.out.println("Er det en nødstistaion. Ja/Nej");
+//        String uEmc = input.nextLine();
+        //Setup
+        switch (uTime) {
+            case 6,7,8,9,10,11,12,13,14,16:
+                nTime = 1;
+                break;
+            case 17,18,19,20,21,22,23:
+                nTime = 2;
+                break;
+            default:
+                nTime = 3;
+        }
+            System.out.println(nTime);
+
+
+
+
+        }
 
     }
-}
+
